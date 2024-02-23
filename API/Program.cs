bb75c02cdf86c99432ea1cdd99ed289f52001a0b
Melike Sümeyye Soydan <aiakosnyks@outlook.com>
2024-02-23 03:08:47 +0300

    25. mid

--- API/Program.cs
+++ API/Program.cs
@@ -8,13 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
 builder.Services.AddDbContext<DataContext>(opt => 
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.UseHttpsRedirection();
 
