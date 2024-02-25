6545d9fda88720be376b83e3eb020a9b20c3cc30
Melike Sümeyye Soydan <aiakosnyks@outlook.com>
2024-02-25 21:48:16 +0300

    34. start

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,9 +6,7 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers;
 
 
-[ApiController]
-[Route("api/Users")] //api/users
-public class UsersController : ControllerBase
+public class UsersController : BaseApiController
 {
     private readonly DataContext _context;
     public UsersController(DataContext context)
