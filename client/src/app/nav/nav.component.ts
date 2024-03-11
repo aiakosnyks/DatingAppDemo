567a31268941e9da81817757c4e7815077452c08
Melike Sümeyye Soydan <aiakosnyks@outlook.com>
2024-03-12 00:23:15 +0300

    67. end

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,6 +3,7 @@ import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -14,7 +15,8 @@ export class NavComponent implements OnInit {
   //currentUser$: Observable<User | null> = of(null) 
   //loggedIn = false;
 
-  constructor(public accountService: AccountService, private router: Router) { }
+  constructor(public accountService: AccountService, private router: Router,
+   private toastr: ToastrService) { }
 
   ngOnInit():void {
     //this.currentUser$ = this.accountService.currentUser$;
@@ -33,7 +35,7 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe({
       next: _ => this.router.navigateByUrl('/members'),
         //this.loggedIn = true;
-      error: error => console.log(error)
+      error: error => this.toastr.error(error.error)
     })
   }
   logout()
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +12,8 @@ export class RegisterComponent implements OnInit{
   @Output() cancelRegister = new EventEmitter();
   model: any = {}
   
-  constructor(private accountService: AccountService) {}
+  constructor(private accountService: AccountService,
+    private toastr: ToastrService) {}
 
   ngOnInit(): void {
   }
@@ -19,12 +21,11 @@ export class RegisterComponent implements OnInit{
   register() {
     this.accountService.register(this.model).subscribe(
       {
-        next: repsonse => {
-          this.cancel();
+        next: () => {
+          this.cancel()
         },
-        error: error => console.log(error)
-      }
-    );
+        error: error => this.toastr.error(error.error)
+      })
   }
 
   cancel()
