deb576826429795fcee79d2ab5fec8951344f095
Melike Sümeyye Soydan <aiakosnyks@outlook.com>
2024-04-02 00:26:16 +0300

    backup

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 
 @Component({
   selector: 'app-member-card',
   templateUrl: './member-card.component.html',
-  styleUrl: './member-card.component.css'
+  styleUrl: './member-card.component.css',
 })
+
 export class MemberCardComponent implements OnInit {
   @Input() member: Member | undefined;
 
