346259e7d2c319198777f43649aeace5d146ff2f
Melike Soydan <63014774+aiakosnyks@users.noreply.github.com>
2024-04-07 07:00:40 +0300

    Revert "Master"

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,13 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map } from 'rxjs';
 import { User } from '../_models/user';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = environment.apiUrl;
+  baseUrl = 'https://localhost:5001/api/';
   private currentUserSource = new BehaviorSubject<User | null>(null);
   currentUser$ = this.currentUserSource.asObservable();
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -6,9 +6,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-import { MemberCardComponent } from './members/member-card/member-card.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
@@ -18,11 +18,8 @@ import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { TestErrorComponent } from './errors/test-error/test-error.component';
 import { CommonModule } from '@angular/common';           
 import {NgIf} from '@angular/common';
-import { JwtInterceptor } from './_interceptors/jwt.interceptor';
-import { MemberEditComponent } from './members/member-edit/member-edit.component';
-import { TabsModule } from 'ngx-bootstrap/tabs';
-import { NgxSpinnerModule } from 'ngx-spinner';
-import { LoadingInterceptor } from './_interceptors/loading.interceptor';
+
+
 
 @NgModule({
   declarations: [
@@ -31,7 +28,7 @@ import { LoadingInterceptor } from './_interceptors/loading.interceptor';
     HomeComponent,
     RegisterComponent,
     MemberListComponent,
-    MemberCardComponent,
+    MemberDetailComponent,
     ListsComponent,
     MessagesComponent, 
   ],
@@ -39,22 +36,16 @@ import { LoadingInterceptor } from './_interceptors/loading.interceptor';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
     BrowserAnimationsModule,
     SharedModule,
     TestErrorComponent,
     CommonModule,
-    MemberDetailComponent,
-    TabsModule,
-    MemberEditComponent,
-    NgIf,
-    NgxSpinnerModule
+    NgIf
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
-    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
-    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
   ],
-  bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
