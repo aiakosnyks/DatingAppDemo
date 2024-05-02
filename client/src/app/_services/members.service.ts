7006521e4e93ecdbd3e7943aadb5f1ba7f384706
Melike Soydan <63014774+aiakosnyks@users.noreply.github.com>
2024-05-02 18:05:40 +0300

    166. end

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -12,11 +12,15 @@ import { map, of } from 'rxjs';
 export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
+  memberCache = new Map();
 
   constructor(private http: HttpClient) { }
 
   getMembers(userParams: UserParams) {
-    console.log(Object.values(userParams).join('-'))
+    const response = this.memberCache.get(Object.values(userParams).join('-'));
+
+    if(response) return of(response);
+
     let params = this.getPaginationHeaders(userParams.pageNumber, userParams.pageSize);
 
     params = params.append('minAge', userParams.minAge);
@@ -24,7 +28,12 @@ export class MembersService {
     params = params.append('gender', userParams.gender);
     params = params.append('orderBy', userParams.orderBy);
 
-    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params);
+    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params).pipe(
+      map(response => {
+        this.memberCache.set(Object.values(userParams).join('-'), response);
+        return response;
+      })
+    )
   }
 
   getMember(username: string) {
