d004c76fe926fde7051b69762579e3eaed7dc68c
Melike Soydan <63014774+aiakosnyks@users.noreply.github.com>
2024-05-06 18:18:58 +0300

    183. end

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -2,6 +2,7 @@ using API.DTOs;
 using API.Entities;
 using API.Extensions;
 using AutoMapper;
+using Namespace;
 
 namespace API.Helpers;
 public class AutoMapperProfiles : Profile
@@ -15,5 +16,11 @@ public class AutoMapperProfiles : Profile
         CreateMap<Photo, PhotoDTO>();
         CreateMap<MemberUpdateDTO, AppUser>();
         CreateMap<RegisterDTO, AppUser>();
+        CreateMap
+        <Message, MessageDTO>()
+            .ForMember(d => d.SenderPhotoUrl, o => o.MapFrom(s => s.Sender.Photos
+                .FirstOrDefault(x => x.IsMain).Url))
+            .ForMember(d => d.RecipientPhotoUrl, o => o.MapFrom(s => s.Recipient.Photos
+                .FirstOrDefault(x => x.IsMain).Url));
     }
 }
