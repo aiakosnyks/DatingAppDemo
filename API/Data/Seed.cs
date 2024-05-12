01aed0ec32e943420959b2a5a461367ec03faa7e
Melike Soydan <63014774+aiakosnyks@users.noreply.github.com>
2024-05-12 23:56:28 +0300

    202. end

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -18,12 +18,8 @@ public class Seed
         var users =  JsonSerializer.Deserialize<List<AppUser>>(userData, options);
 
         foreach(var user in users)
-        {
-            using var hmac = new HMACSHA512();
-            
+        {            
             user.UserName = user.UserName.ToLower();
-            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
-            user.PasswordSalt = hmac.Key;
 
             context.Users.Add(user);
         }
